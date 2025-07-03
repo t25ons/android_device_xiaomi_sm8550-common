3d6ec5dc4c506d36e123422efbf2c96de2497b72
fukiame <fukiame@proton.me>
2025-07-03 16:16:16 +0700

    Revert "socrates: Set lineage version as fingerprint for mtdoops"
    This reverts commit ee6141874a0197ac583aa51bac0128291023ebfc.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,7 +71,7 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 disable_dma32=on swinfo.fingerprint=$(LINEAGE_VERSION) mtdoops.fingerprint=$(LINEAGE_VERSION)
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 disable_dma32=on swinfo.fingerprint=socrates:13/V14.0.25.0.TMKCNXM:user mtdoops.fingerprint=socrates:13/V14.0.25.0.TMKCNXM:user
 
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.console=ttyMSM0
 
